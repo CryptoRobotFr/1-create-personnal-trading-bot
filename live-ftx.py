04d6abeddf5b72665a5eb9f61b9a5ec62d814ade
Crypto Robot <88462650+CryptoRobotFr@users.noreply.github.com>
2021-08-05 00:26:38 +0200

    Create live-ftx.py

---
+++ live-ftx.py
@@ -0,0 +1,63 @@
+import ftx
+import pandas as pd
+import ta
+import time
+import json
+from math import *
+
+accountName = 'Ytb-Tester'
+pairSymbol = 'BTC/USD'
+fiatSymbol = 'USD'
+cryptoSymbol = 'BTC'
+myTruncate = 4
+
+client = ftx.FtxClient(api_key='xL6SYTqmHtawoCaihqkoqlCtW_q_vfJ6-xVZlnh6',
+                   api_secret='em2tgTy47IblAubmT18_yIAMwzDVXoTnM2hHCvWm', subaccount_name=accountName)
+
+data = client.get_historical_data(
+    market_name=pairSymbol, 
+    resolution=3600, 
+    limit=650, 
+    start_time=float(
+    round(time.time()))-650*3600, 
+    end_time=float(round(time.time())))
+df = pd.DataFrame(data)
+
+df['SMA200'] = ta.trend.sma_indicator(df['close'], 200)
+df['SMA600'] = ta.trend.sma_indicator(df['close'], 600)
+print(df)
+
+def getBalance(myclient, coin):
+    jsonBalance = myclient.get_balances()
+    pandaBalance = pd.DataFrame(jsonBalance)
+    if pandaBalance.loc[pandaBalance['coin'] == coin].empty : return 0
+    else : return float(pandaBalance.loc[pandaBalance['coin'] == coin]['free'])
+
+def truncate(n, decimals=0):
+    r = floor(float(n)*10**decimals)/10**decimals
+    return str(r)
+    
+
+actualPrice = df['close'].iloc[-1]
+fiatAmount = getBalance(client, fiatSymbol)
+cryptoAmount = getBalance(client, cryptoSymbol)
+print(actualPrice, fiatAmount, cryptoAmount)
+
+if float(fiatAmount) > 5 and df['SMA200'].iloc[-2] > df['SMA600'].iloc[-2]:
+    quantityBuy = truncate(float(fiatAmount)/actualPrice, myTruncate)
+    buyOrder = client.place_order(
+        market=pairSymbol, 
+        side="buy", 
+        price=None, 
+        size=quantityBuy, 
+        type='market')
+    print(buyOrder)
+
+if float(cryptoAmount) > 0.0001 and df['SMA200'].iloc[-2] < df['SMA600'].iloc[-2]:
+    buyOrder = client.place_order(
+        market=pairSymbol, 
+        side="sell", 
+        price=None, 
+        size=truncate(cryptoAmount, myTruncate), 
+        type='market')
+    print(buyOrder)
